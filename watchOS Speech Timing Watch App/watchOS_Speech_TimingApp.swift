e99239f597effb8d7d9f08649d4eadedce918653
Creeon <creeon99@gmail.com>
2024-08-28 23:19:27 -0500

    Initial Commit

---
+++ watchOS Speech Timing Watch App/watchOS_Speech_TimingApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOS_Speech_TimingApp.swift
+//  watchOS Speech Timing Watch App
+//
+//  Created by Andrew Culbertson on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Speech_Timing_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
